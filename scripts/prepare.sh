405a4865c02f67ede647a10bb3220086b5cefc12
Jose Sanz <jsanz@cyracom.com>
2024-03-26 10:39:32 +0000

    added diagrams dir to gitignore

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,6 +3,16 @@
 sudo apt-get update
 sudo apt-get install graphviz -y
 
+# Check if the 'diagrams' directory exists
+if [ -d "diagrams" ]; then
+    echo "Directory 'diagrams' already exists."
+else
+    echo "Creating directory 'diagrams'..."
+    mkdir diagrams
+    echo "Directory 'diagrams' created successfully."
+fi
+
+# Check if the virtual environment 'venv' exists
 if [ -d "venv" ]; then
     echo "Virtual environment 'venv' already exists."
 else
@@ -11,4 +21,5 @@ else
     echo "Virtual environment 'venv' created successfully."
 fi
 
+# Install Python dependencies
 pip install -r requirements.txt
