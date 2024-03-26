2e87eb00d330186d2980fc61873bda1303096843
Jose Sanz <jsanz@cyracom.com>
2024-03-26 07:41:28 +0000

    more initial files

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,4 +2,13 @@
 
 sudo apt-get update
 sudo apt-get install graphviz -y
+
+if [ -d "venv" ]; then
+    echo "Virtual environment 'venv' already exists."
+else
+    echo "Creating virtual environment 'venv'..."
+    python3 -m venv venv
+    echo "Virtual environment 'venv' created successfully."
+fi
+
 pip install -r requirements.txt
